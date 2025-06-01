e6dd524e7e558eec49e72ce8fbb71f279fc29986
rodrig-crzz <rodrigow1238@gmail.com>
2025-06-01 16:37:54 -0300

    :fire:

--- services/accounts/tokenObtainPair.js
+++
@@ -1,6 +0,0 @@
-import apiAccounts from "./apiAccounts"
-
-export default async function tokenObtainPair({ login, password }) {
-  const response = await apiAccounts.post('token/', { login, password })
-  return response.data
-}
